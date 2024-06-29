b0d96e23d5cbd41389f57706f083a5188a2e8f84
yuu <2811yu0914@gmail.com>
2024-06-29 16:00:40 +0900

    記事を削除できるようにした

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  
   def index
     @articles = Article.all
   end
@@ -32,6 +33,12 @@ class ArticlesController < ApplicationController
       flash.now[:error] = '更新できませんでした'
       render :edit
     end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
   end
 
 
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,8 @@
+// require jquery
+// require jquery_ujs
+
 import { Application } from "@hotwired/stimulus"
+import "controllers"
 
 const application = Application.start()
 
@@ -7,3 +11,8 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// app/javascript/controllers/application.js
+import Rails from "@rails/ujs";
+Rails.start();
+
