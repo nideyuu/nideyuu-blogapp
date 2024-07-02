eef14d2df700d925df105ec4b92de69b045bef6d
yuu <2811yu0914@gmail.com>
2024-07-02 12:01:52 +0900

    Add application.js to Git tracking

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Rails from "@rails/ujs"
+
+Rails.start()
+
+const application = Application.start()
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+
+
